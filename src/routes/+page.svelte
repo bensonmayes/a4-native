833d8f998cb7e8ee44bd15b8f6cd9ef4d434b0b3
Bensonmayes <bmayes@confederationcollege.ca>
2024-03-19 14:57:36 -0400

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import '../style.css'
+	let todoItem= '';
+	let todoList = [];
+
+	function addToArray() {
+		if (todoItem =='') {
+			return;
+		}
+		todoList.push(todoItem);
+		todoList = todoList;
+		console.log(todoList);
+		todoItem = '';
+	}
 </script>
 
 <svelte:head>
@@ -9,51 +19,13 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<h1>todoapp!</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+<form on:submit|preventDefault={addToArray}>
+	<input type="text" bind:value={todoItem}>
+	<button type="submit">Add</button>
+</form>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+<ul>
 
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+</ul>
